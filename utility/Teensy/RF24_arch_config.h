ddb4c2804c7bd55529a2fea5d2938329f3cfe037
Alan Stein <alan@drone-combat.com>
2022-03-23 11:42:17 -0400

    Replace #define printf with #include "LibPrintf.h"
    This #define caused a lot of breakage wrt other libraries, such as SD and FS. However, using LibPrintf.h could have side-effects in the future -- it's not totally clear; we'll have to keep an eye on it.

--- utility/Teensy/RF24_arch_config.h
+++ utility/Teensy/RF24_arch_config.h
@@ -19,7 +19,9 @@
 #define _SPI SPIClass
 #define RF24_SPI_PTR
 
-#define printf Serial.printf
+#include "LibPrintf.h"
+// #pragma message("Including LibPrintf via modified RF24 library")
+// #define printf Serial.printf -- causes breakage
 
 #ifdef SERIAL_DEBUG
     #define IF_SERIAL_DEBUG(x) ({x;})
